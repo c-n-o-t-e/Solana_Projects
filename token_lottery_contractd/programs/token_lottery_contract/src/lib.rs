43c6cfcb3f6668b74d3dfe983ff6a0100d662e2c
c-note <dongustavo20@gmail.com>
2025-01-07 11:54:59 +0000

    Add ClaimPrize struct and function.

--- token_lottery_contractd/programs/token_lottery_contract/src/lib.rs
+++ token_lottery_contractd/programs/token_lottery_contract/src/lib.rs
@@ -303,6 +303,106 @@ pub mod token_lottery {
 
         Ok(())
     }
+
+    pub fn claim_prize(ctx: Context<ClaimPrize>) -> Result<()> {
+        // Check if winner has been chosen
+        msg!(
+            "Winner chosen: {}",
+            ctx.accounts.token_lottery.winner_chosen
+        );
+        require!(
+            ctx.accounts.token_lottery.winner_chosen,
+            ErrorCode::WinnerNotChosen
+        );
+
+        // Check if token is a part of the collection
+        require!(
+            ctx.accounts.metadata.collection.as_ref().unwrap().verified,
+            ErrorCode::NotVerifiedTicket
+        );
+        require!(
+            ctx.accounts.metadata.collection.as_ref().unwrap().key
+                == ctx.accounts.collection_mint.key(),
+            ErrorCode::IncorrectTicket
+        );
+
+        let ticket_name = NAME.to_owned() + &ctx.accounts.token_lottery.winner.to_string();
+        let metadata_name = ctx.accounts.metadata.name.replace("\u{0}", "");
+
+        msg!("Ticket name: {}", ticket_name);
+        msg!("Metdata name: {}", metadata_name);
+
+        // Check if the winner has the winning ticket
+        require!(metadata_name == ticket_name, ErrorCode::IncorrectTicket);
+        require!(
+            ctx.accounts.destination.amount > 0,
+            ErrorCode::IncorrectTicket
+        );
+
+        **ctx
+            .accounts
+            .token_lottery
+            .to_account_info()
+            .try_borrow_mut_lamports()? -= ctx.accounts.token_lottery.lottery_pot_amount;
+        **ctx.accounts.payer.try_borrow_mut_lamports()? +=
+            ctx.accounts.token_lottery.lottery_pot_amount;
+
+        ctx.accounts.token_lottery.lottery_pot_amount = 0;
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct ClaimPrize<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"token_lottery".as_ref()],
+        bump = token_lottery.bump,
+    )]
+    pub token_lottery: Account<'info, TokenLottery>,
+
+    #[account(
+        mut,
+        seeds = [b"collection_mint".as_ref()],
+        bump,
+    )]
+    pub collection_mint: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        seeds = [token_lottery.winner.to_le_bytes().as_ref()],
+        bump,
+    )]
+    pub ticket_mint: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        seeds = [b"metadata", token_metadata_program.key().as_ref(), ticket_mint.key().as_ref()],
+        bump,
+        seeds::program = token_metadata_program.key(),
+    )]
+    pub metadata: Account<'info, MetadataAccount>,
+
+    #[account(
+        associated_token::mint = ticket_mint,
+        associated_token::authority = payer,
+        associated_token::token_program = token_program,
+    )]
+    pub destination: InterfaceAccount<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        seeds = [b"metadata", token_metadata_program.key().as_ref(), collection_mint.key().as_ref()],
+        bump,
+        seeds::program = token_metadata_program.key(),
+    )]
+    pub collection_metadata: Account<'info, MetadataAccount>,
+
+    pub token_program: Interface<'info, TokenInterface>,
+    pub system_program: Program<'info, System>,
+    pub token_metadata_program: Program<'info, Metadata>,
 }
 
 #[derive(Accounts)]
